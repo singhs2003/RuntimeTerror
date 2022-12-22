b74d0f4a79dc46ad6f37665c9fd04183b130bd1c
singhs2003 <114583872+singhs2003@users.noreply.github.com>
2022-12-22 19:59:09 +0530

    Update first_commit.py

--- runtim terror/first_commit.py
+++ runtim terror/first_commit.py
@@ -1,6 +1,37 @@
 !/usr/bin/env python
+"""
+	
+******************************************** PATIENT MONITORING SYSTEM *********************************************                                                                                                                       
+                                                                                                                       
+HHHHHHHHH     HHHHHHHHH                                 AAA               lllllll                                      
+H:::::::H     H:::::::H                                A:::A              l:::::l                                      
+H:::::::H     H:::::::H                               A:::::A             l:::::l                                      
+HH::::::H     H::::::HH                              A:::::::A            l:::::l                                      
+  H:::::H     H:::::H      eeeeeeeeeeee             A:::::::::A            l::::l    ggggggggg   ggggg   ooooooooooo   
+  H:::::H     H:::::H    ee::::::::::::ee          A:::::A:::::A           l::::l   g:::::::::ggg::::g oo:::::::::::oo 
+  H::::::HHHHH::::::H   e::::::eeeee:::::ee       A:::::A A:::::A          l::::l  g:::::::::::::::::go:::::::::::::::o
+  H:::::::::::::::::H  e::::::e     e:::::e      A:::::A   A:::::A         l::::l g::::::ggggg::::::ggo:::::ooooo:::::o
+  H:::::::::::::::::H  e:::::::eeeee::::::e     A:::::A     A:::::A        l::::l g:::::g     g:::::g o::::o     o::::o
+  H::::::HHHHH::::::H  e:::::::::::::::::e     A:::::AAAAAAAAA:::::A       l::::l g:::::g     g:::::g o::::o     o::::o
+  H:::::H     H:::::H  e::::::eeeeeeeeeee     A:::::::::::::::::::::A      l::::l g:::::g     g:::::g o::::o     o::::o
+  H:::::H     H:::::H  e:::::::e             A:::::AAAAAAAAAAAAA:::::A     l::::l g::::::g    g:::::g o::::o     o::::o
+HH::::::H     H::::::HHe::::::::e           A:::::A             A:::::A   l::::::lg:::::::ggggg:::::g o:::::ooooo:::::o
+H:::::::H     H:::::::H e::::::::eeeeeeee  A:::::A               A:::::A  l::::::l g::::::::::::::::g o:::::::::::::::o
+H:::::::H     H:::::::H  ee:::::::::::::e A:::::A                 A:::::A l::::::l  gg::::::::::::::g  oo:::::::::::oo 
+HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeeeAAAAAAA                   AAAAAAAllllllll    gggggggg::::::g    ooooooooooo   
+                                                                                              g:::::g                  
+                                                                                  gggggg      g:::::g                  
+                                                                                  g:::::gg   gg:::::g                  
+                                                                                   g::::::ggg:::::::g                  
+                                                                                    gg:::::::::::::g                   
+                                                                                      ggg::::::ggg                     
+                                                                                         gggggg                        
+                                                                                         
+***********************************************************************************************************************************
+Machine learning algortihm to read data from patient and monitor them constantly and if the stats are in the danger zone 
+it return a value 1 which will be deocoded and immediately conveyed to doctor and staff on duty.
+************************************************************************************************************************************
 
-#------------------------------------------------------------
 ##############################################################     PATIENT MONITORING SYSTEM     #######################################################################
 #MADE BY :SAKSHAM SINGH, MAIT , 2nd Yr.
 #        :ISHAAN SANGWAN, MAIT , 2nd Yr.
@@ -8,6 +39,7 @@
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
                                         """ This code cleans the data set we got from kaggle of  1000+ records"""
+"""
 #importing libraries.
 import pandas as pd
 import numpy as np
