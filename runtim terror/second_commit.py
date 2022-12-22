88d15d9866ab8c0b662838a039398a947f279e65
singhs2003 <114583872+singhs2003@users.noreply.github.com>
2022-12-22 19:57:00 +0530

    Update and rename seconf_commit.py to second_commit.py

--- runtim terror/second_commit.py
+++ runtim terror/second_commit.py
@@ -28,7 +28,7 @@ HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeeeAAAAAAA                   AAAAAAAllllll
                                                                                          
 ***********************************************************************************************************************************
 Machine learning algortihm to read data from patient and monitor them constantly and if the stats are in the danger zone 
-it return a value 1 which will be later read and take proper measures to inform the staff
+it return a value 1 which will be deocoded and immediately conveyed to doctor and staff on duty.
 ************************************************************************************************************************************
 MADE BY :
 	SAKSHAM SINGH, MAIT
@@ -39,50 +39,26 @@ Date: 	22/12/2022
 	
 """
 import first_commt
+
 from sklearn.model_selection import train_test_split  
 
 #to split array or data in train and test part
-
-
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
 
 
 
-# to create pipeline output of one step as input in nxt step , diff classificatn models
-#from sklearn import preprocessing
-
+#Lets's import certain libraries tpo standardize data and models to power our algo.
 from sklearn.preprocessing import StandardScaler
-
-# It standardizes features by subtracting the mean value from the feature and then dividing the result by feature standard deviation.i.e
-#values differ in range or are in different units.
-
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-
-
-# grouping of items eclidean distance column in table assume k=3
-
 from sklearn.svm import SVC
-
-# sets plane between two categories so that dist between pts of both categories is max
-
 from sklearn.tree import DecisionTreeClassifier
-
-# classes may not be seperable by a linear line , it decides line by fact that try to maximizes inf ogain we want pure node, calculates entroy
-#info gain entropy of prent - entropy of child so higher info gain is favoured and model compares every possible
-#to max info gain splits recursively to 
-
 from sklearn.ensemble import RandomForestClassifier
-
-# better than decision tree 
-
 from sklearn.ensemble import GradientBoostingClassifier
-
+#Importing pipeline module for pushing data sequentially.
 from sklearn.pipeline import Pipeline
 
-
-#  creating a machine learning pipeline for standarising the raw data 
+#  creating a machine learning pipeline along with  standarising the raw data 
 
 
 pipeline_lr=Pipeline([('scalar1',StandardScaler()),('lr_classifier',LogisticRegression())])
@@ -92,29 +68,24 @@ pipeline_dt=Pipeline([('dt_classifier',DecisionTreeClassifier())])
 pipeline_rf=Pipeline([('rf_classifier',RandomForestClassifier())])
 pipeline_gbc=Pipeline([('gbc_classifier',GradientBoostingClassifier())])
 
-
-# list for pipelines
+# list of pipelines
 pipelines=[pipeline_lr,pipeline_knn,pipeline_svc,pipeline_dt,pipeline_rf,pipeline_gbc]
-
 
 pipelines
 
-# train pipelines
+# train models which are recognized by pipes.
 for pipe in pipelines:
     pipe.fit(X_train,Y_train)
     
-
+#Pipeline Dictionary
 pipe_dict={0:'LR',1:'KNN',2:'SVC',3:'DT',4:'RF',5:'GBC'}
-
-
-# pipe_dict 
-# Logisticregression,KNeighborsClassifier,SVC,DecisionTreeClassifier,RandomForestClassifier,GradientBoostingClassifier
 
 
 for i,model in enumerate(pipelines):
     print("{}Test Accuracy:{}".format(pipe_dict[i],model.score(X_test,Y_test)*100))
+#This will give us the accuracy of all models
 
-
+#RandonForest comes out as best option with accuracy of about 84%.
 from sklearn.ensemble import RandomForestClassifier
 
 X=data.drop('outcome',axis=1)
@@ -122,9 +93,5 @@ Y=data['outcome']
 
 rf=RandomForestClassifier()
 
-
+#Now we trained it for thw whole data
 rf.fit(X,Y)
-
-
-data.columns
-
